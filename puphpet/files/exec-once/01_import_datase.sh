14abd459ae9bdf629e555b1cafd3454669a911bf
Peter Böthig <p.boethig@ibrams.com>
2016-02-14 22:29:41 +0100

    add sampledata

--- puphpet/files/exec-once/01_import_datase.sh
+++ puphpet/files/exec-once/01_import_datase.sh
@@ -2,7 +2,7 @@
 storagePath=/vagrant/puphpet/files/databases
 backupStorage="$storagePath/backup"
 
-#set enviroment to deb
+#set enviroment to dev
 enviroment=dev
 
 #type iniconfig -a to get all configitems. Type ./getconfig -h for help
@@ -11,42 +11,28 @@ password=`iniconfig $enviroment database password`
 databasename=`iniconfig $enviroment database name`
 
 #if samopledata installation was activated (@see config.ini)
-if [ "`iniconfig $enviroment sampledata install`" == "1" ]
+#core database
+if [ -f "$storagePath/database.sql.gz" ]
 then
-    #core database
-    if [ -f "$storagePath/database.sql.gz" ]
-    then
 
-        echo "##########################Install Sampledata for magento#####################################"
+    echo "########################## Install core + sampledata for magento #####################################"
 
-        echo "backup current database under:$backupStorage"
+    echo "backup current database under:$backupStorage/database_bak`date`.sql.gz"
 
-        mysqldump -u$username -p$password $databasename | gzip >"/$backupStorage/database_bak.sql.gz"
+    mysqldump -u$username -p$password $databasename | gzip >"/$backupStorage/database_bak.`date`.sql.gz"
 
-        mysql -u $username -p$password -e "DROP DATABASE IF EXISTS $databasename"
+    mysql -u $username -p$password -e "DROP DATABASE IF EXISTS $databasename"
 
-        mysql -u$username -p$password -e "CREATE DATABASE $databasename"
+    mysql -u$username -p$password -e "CREATE DATABASE $databasename"
 
-        echo "unpacking dump under:$storagePath/database.sql"
+    echo "unpacking dump under:$storagePath/database.sql"
 
-        gunzip -c $storagePath/database.sql.gz > $storagePath/database.sql
+    gunzip -c $storagePath/database.sql.gz > $storagePath/database.sql
 
-        echo "importing dump under: $storagePath/database.sql.gz"
+    echo "importing dump under: $storagePath/database.sql.gz"
 
-        mysql -u$username -p$password $databasename <$storagePath/database.sql
+    mysql -u$username -p$password $databasename <$storagePath/database.sql
 
-        rm -rf $storagePath/database.sql
-
-
-        # copy sampledatacodebase to magento root
-        echo "copiing sampledata to magentoroot"
-
-        sourcePath=/vagrant/puphpet/files/magentosampledata/*
-        targetPath=`iniconfig $enviroment filesystem documentroot`
-        echo "sampledatapath: $sourcePath"
-        echo "targetPath: $targetPath"
-
-        cp -fr $sourcePath $targetPath
-    fi
+    rm -rf $storagePath/database.sql
 fi
 
